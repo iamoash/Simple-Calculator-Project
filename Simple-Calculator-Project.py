a33d9bc16820cda1a1fd3e147b08c90da81181e3
Joash <guanzonjoashmatthew@gmail.com>
2024-05-07 22:21:54 +0800

    Added Pseudocodes

---
+++ Simple-Calculator-Project.py
@@ -0,0 +1,15 @@
+# Perform the selected operation
+
+#Display the menu with the available operations (Addition, Subtraction, Multiplication, Division).
+
+# Ask the user to choose an operation 
+
+# If the choice is invalid, display an error message and go back to the first step.
+
+# Ask the user for two numbers
+
+# Display the result.
+
+# Ask the user if they want to perform another calculation.
+
+# Handle Exceptions
